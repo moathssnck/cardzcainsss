44945dccfe0834d35b60d764b7c8bde73ec0047b
moathssnck <myneww00@gmail.com>
2025-07-29 13:04:04 +0300

    sss

--- lib/firebasee.ts
+++ lib/firebasee.ts
@@ -10,13 +10,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6fMi5ZQZGLG-AWW2l59eexC__TTl1VaQ",
-  authDomain: "zaimns.firebaseapp.com",
-  projectId: "zaimns",
-  storageBucket: "zaimns.firebasestorage.app",
-  messagingSenderId: "573438865337",
-  appId: "1:573438865337:web:4302dc325aff0e63fd9068",
-  measurementId: "G-4R9XW48TV6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
